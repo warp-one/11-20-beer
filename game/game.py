c0c9966a520fb4467f880539a43a8d4a1e95af03
Michael Schuller <chick@mschuller.net>
2015-11-20 21:59:27 -0500

    Add flavor text

--- game/game.py
+++ game/game.py
@@ -9,15 +9,25 @@ class Game(object):
         self.player = player.Player(img=resources.player, x=50, y=50, batch=self.batch)
         self.window.push_handlers(self.player)
         self.window.push_handlers(self.on_draw)
+        self.window.push_handlers(self.on_key_press)
+
+        self.label_text = "The rag and bone man did not come today..."
+        self.label_visible = True
+
+    def on_key_press(self, symbols, modifiers):
+    	if self.label_visible:
+    		self.label_visible=False
         
     def on_draw(self):
+    	from text import GameText # FIXME: the suck
         self.window.clear()
         self.batch.draw()
+        if self.label_visible:
+        	self.label = GameText(self.label_text)
+        	self.label.draw()
         
     def execute(self):
         pyglet.app.run()
         
     
-if __name__ == '__main__':
-    game = Game()
-    game.execute()
+game = Game()
