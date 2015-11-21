9dcb8508a84f209b401eaa1d54375005e6483ded
William Schuller <wrschuller@gmail.com>
2015-11-20 21:38:40 -0500

    mvmt

--- game/player.py
+++ game/player.py
@@ -1,8 +1,21 @@
 import pyglet
+from pyglet.window import key
+
 
 class Player(pyglet.sprite.Sprite0):
+
+    step = 5
+
     def __init__(self, *args, **kwargs):
         super(Player, self).__init__(*args, **kwargs)
         
     def on_key_press(self, symbol, modifiers):
-        pass
+        if symbol == key.UP:
+            self.y += self.step
+        if symbol == key.DOWN:
+            self.y -= self.step
+        if symbol == key.LEFT:
+            self.x -= self.step
+        if symbol == key.RIGHT:
+            self.x += self.step
+        
