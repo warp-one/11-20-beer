c21afb08f124949f7e8d38c5c6658120818ff6ca
Michael Schuller <chick@mschuller.net>
2015-11-20 22:47:14 -0500

    moar words

--- game/enemy.py
+++ game/enemy.py
@@ -2,13 +2,18 @@ from random import randint
 
 import pyglet
 
+from text import EnemyText
+
 class Enemy(pyglet.sprite.Sprite):
 
     step = 5
 
     def __init__(self, *args, **kwargs):
         super(Enemy, self).__init__(*args, **kwargs)
+        self.text = EnemyText(self)
         
     def update(self, dt):
         self.x += randint(-self.step, self.step)
         self.y += randint(-self.step, self.step)
+        self.text.x = self.x
+        self.text.y = self.y
